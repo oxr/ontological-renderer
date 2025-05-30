f6726f28e61380415a8fd3a4e35385e2ee18cc07
Ondrej Rypacek <oxr@Macmini.lan>
2025-05-30 16:37:26 +0100

    working on the rendering pipeline

--- src/Scene.hs
+++ src/Scene.hs
@@ -2,21 +2,32 @@ module Scene where
 
 import Vector
 
-data Ray = MkRay { origin :: Vec , dir :: Vec }
+
+data Ray = Ray Vec Vec 
             deriving Show
 
+origin :: Ray -> Vec
+origin (Ray v _ )= v
+
+direction :: Ray -> Vec
+direction (Ray _ v) = v
+
+
 mkNormalRay :: Vec -> Vec -> Ray
-mkNormalRay o d = MkRay { origin = o, dir = normalize d }
+mkNormalRay o d = Ray o (normalize d)
 
 
 
-data Plane = MkPlane { point :: Vec , normal :: Vec }
+data Plane = MkPlane Vec Vec
 
 
 data Object = Sphere Vec Double
 
+newtype Light = Light { mainSource :: Vec }
 
-type Scene = [Object]
+
+-- at this point light is just a direction from an infinite distance
+data Scene = Scene { objects:: [Object] , light :: Light } 
 
 
 -- intersection of Ray and Object gives a point a normal, ie. a Ray
@@ -27,7 +38,7 @@ sq :: Num a => a -> a
 sq x = x * x
 
 intersectSphere :: Vec -> Double -> Ray ->  [Ray]
-intersectSphere c r (MkRay o d) =
+intersectSphere c r (Ray o d) =
     let
       oc = o |-| c
       a  = d |.| d
@@ -43,7 +54,7 @@ intersectSphere c r (MkRay o d) =
             normalRay t =
               let p = o |+| (t |.*| d)
                   n = normalize (p |-| c)
-              in MkRay p n
+              in Ray p n
         in if disc == 0
              then [normalRay (-b)]
              else [normalRay t1, normalRay t2]
@@ -51,5 +62,40 @@ intersectSphere c r (MkRay o d) =
 
 
 
+-- when we collect all intersections, for all rays, 
+-- for each intersection which is a colletion of Rays, light model calculates
+-- in a given scene, for a given point and direction the 
+-- assumptions: Ray is normal , Light vector is normal
+lightingModel :: Scene -> Ray -> Double
+lightingModel (Scene _ (Light ms)) = lambert (vecNeg ms )
+                where 
+                    lambert :: Vec -> Ray -> Double
+                    lambert v (Ray _ d)  = v `dot` d
 
 
+-- now just collect all intersections for all rays , calculate their light value and you are done. 
+-- you can also generate a text preview
+
+
+-- from focal length, x half resolution, y half resolution, give me all rays
+-- a matrix of rays
+rays :: Double -> Double -> Double -> [[Ray]]
+rays f dx dy = [ [ mkRay x y | x <- [-dx .. dx] ] | y <- [-dy .. dy] ]
+                where mkRay x y = Ray [0,0,0] [x,y,f]
+
+
+-- one test scene
+scene1 :: Scene
+scene1 = Scene { objects = [Sphere [0, 0, 100] 25], light = Light { mainSource = normalize [- 1, - 1, 1] } }
+
+take1st :: [a] -> [a]
+take1st [] = []
+take1st (h:_) = [h]
+
+-- now calculate the light
+showScene :: Scene -> [Ray] -> [Double]
+showScene s rs = let objectIntersectors = map intersect (objects s)
+                     intersects = concatMap take1st [ oi r | oi <- objectIntersectors , r <- rs]
+                   in map (lightingModel s) intersects
+ 
+                                    
