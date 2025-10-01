b67b888b3b690c74d8e78eace5bcb0a9d88d0243
Ondrej Rypacek <ondrej.rypacek@gmail.com>
2025-10-01 20:19:44 +0100

    Moved form app to be able to cabal repl it

---
+++ src/CLParser.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+
+module CLParser where
+
+import Parser
+import Scene
+import Vector
+
+
+
+parseCam :: Parser (Int, Int, Int)
+parseCam = triple int int int
+
+
+parseSphere :: Parser Object
+parseSphere = do 
+    ((x,y,z),r, sface) <- triple (triple double double double) double surface
+    return $ Object (Sphere (V x y z) r) sface
+
+parsePlane :: Parser Object
+parsePlane = do
+                            ((x,y,z),(a,b,c), r) <- triple (triple double double double) (triple double double double) surface
+                            return $ Object (Plane (Ray (V x y z) (V a b c))) r
+
+
+
+
+surface :: Parser (Double, Rgb)
+surface = pair double rgb
+
+
+rgb :: Parser Rgb
+rgb = do (r,g,b) <- triple double double double  
+         return $ V r g b
+
+cmyk :: Parser Cmyk
+cmyk = quadruple int int int int
+
+
+
+parseObject :: Parser Object
+parseObject = do 
+                ch <- upper
+                case ch of
+                    'S' -> parseSphere
+                    'P' -> parsePlane 
+                    _ -> zero
+
+                 
+
+parseScene :: Parser [Object]
+parseScene = list parseObject 
+
+parseLight :: Parser Light
+parseLight = do { (x,y,z) <- triple double double double ; return $ Light (V x y z) 0 }
+
+
+
+
+
